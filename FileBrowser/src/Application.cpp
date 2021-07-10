633e5fa45e3c149e733669b6c9122cad262f4c5c
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-07-10 15:46:37 -0600

    inching forward

--- FileBrowser/src/Application.cpp
+++ FileBrowser/src/Application.cpp
@@ -16,9 +16,9 @@ void Application::run() {
       TileView::Create(top_tile_view).configure([](TileView &obj, void *) {
         obj.add_tile(
                TileView::Location().set_column(0).set_row(0).set_direction(
-                   Direction::right | Direction::bottom),
+                   Direction::right),
                [](Container &obj) {
-                 obj.add<Folder>(Folder::Create("folder").set_path("/").configure(Folder::configure));
+                 obj.add<Folder>(Folder::Create("folder").set_path("/dev").configure(Folder::configure));
                })
             .add_tile(
                 TileView::Location().set_column(0).set_row(1).set_direction(
@@ -26,13 +26,6 @@ void Application::run() {
                 [](Container &obj) {
                   obj.add<Label>(Label::Create("label").configure(
                       [](Label &obj, void *) { obj.set_text("0,1"); }));
-                })
-            .add_tile(
-                TileView::Location().set_column(1).set_row(0).set_direction(
-                    Direction::top),
-                [](Container &obj) {
-                  obj.add<Label>(Label::Create("label").configure(
-                      [](Label &obj, void *) { obj.set_text("1,0"); }));
                 });
       }));
 
--- FileBrowser/src/Folder.cpp
+++ FileBrowser/src/Folder.cpp
@@ -1,28 +1,58 @@
 #include <fs.hpp>
+#include <lvgl.hpp>
 
 #include "Folder.hpp"
 
-Folder::Folder(Object parent, const Create& options){
-  m_object = api()->list_create(parent.object());
+Folder::Folder(Object parent, const Create &options) {
+  m_object = api()->obj_create(parent.object());
   set_name(options.name());
   m_path = options.path();
 }
 
+void Folder::initialize() {
 
-void Folder::initialize(){
+  set_layout(*Api::api()->layout_flex)
+      .set_flex_flow(FlexFlow::column)
+      .set_top_padding(0)
+      .set_border_width(0)
+      .set_bottom_padding(0)
+      .set_left_padding(0)
+      .set_right_padding(0)
+      .set_width(100_percent)
+      .set_height(100_percent);
 
   printf("size of event:%d\n", sizeof(lv_event_code_t));
-  if( FileSystem().directory_exists(m_path) == false ){
+  if (FileSystem().directory_exists(m_path) == false) {
     send_alert(get_parent());
     return;
   }
 
-  //load the path
-  const auto list = FileSystem().read_directory(m_path);
+  add<Label>(Label::Create("FolderTitle")
+                 .configure(this, [](Label &label, void *context) {
+                   Folder *self = reinterpret_cast<Folder *>(context);
 
-  //add items in the director to the list
-  for(const auto & item: list){
-    printf("adding %s\n", item.cstring());
-    api()->list_add_text(object(), item.cstring());
-  }
+                   label.set_height(15_percent)
+                       .set_width(100_percent)
+                       .set_text(self->m_path.cstring());
+                 }));
+
+  add<lv::List>(lv::List::Create("folderList")
+                    .configure(this, [](lv::List &list, void *context) {
+                      // load the path
+                      Folder *self = reinterpret_cast<Folder *>(context);
+                      const auto file_list =
+                          FileSystem().read_directory(self->m_path);
+
+                      list.set_top_padding(0)
+                          .set_border_width(0)
+                          .set_bottom_padding(0)
+                          .set_width(100_percent)
+                          .set_height(80_percent);
+
+                      // add items in the director to the list
+                      for (const auto &item : file_list) {
+                        printf("adding %s\n", item.cstring());
+                        list.add_button("", item.cstring());
+                      }
+                    }));
 }
